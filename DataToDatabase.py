9b66fa3a9845c77133dd8f7c49837ae41c3fbeb1
Bhavesh Gupta <122150518+Azufendei@users.noreply.github.com>
2024-11-04 13:13:53 +0530

    Update DataToDatabase.py

--- DataToDatabase.py
+++ DataToDatabase.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import db
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL':"https://security-with-face-recognition-default-rtdb.firebaseio.com/"
+    'databaseURL':"" #add your url here
 })
 
 ref = db.reference('Personnel')
